85cc9e85cbf68108d56176fd48d6d2189233f55b
Indian <Indian@DESKTOP-VK0CSFB>
2023-11-03 10:50:00 +0530

    Add project files.

---
+++ statemanagete/Controllers/ReadCookiesSessionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace statemanagete.Controllers
+{
+    public class ReadCookieSessionController : Controller
+    {
+        private readonly IHttpContextAccessor _httpcontextAccessor;
+
+        public ReadCookieSessionController(IHttpContextAccessor httpcontextAccessor)
+        {
+            _httpcontextAccessor = httpcontextAccessor;
+        }
+
+        public IActionResult ReadCookie()
+        {
+            if (_httpcontextAccessor.HttpContext != null)
+            {
+                //read value from cookie
+                ViewBag.Email = _httpcontextAccessor.HttpContext.Request.Cookies["email"];
+            }
+            return View();
+        }
+        public IActionResult ReadSession()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("email");
+            //string role=HttpContext.Session.GetString("roleId");
+            return View();
+        }
+
+    }
+}
+
